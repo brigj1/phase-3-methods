77eb0c33a38de440a4a8b2447991fbc21725a099
Brigham <brig.johnson@gmail.com>
2022-06-05 00:30:34 -0400

    commit tree 4db059fec6a51fbcb3850b6b01353776d97923c6

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def halve(number)
+  #if (number.typeof != "number") return nil;
+  #if (! number.to_i)
+    #return nil
+  #end
+  #unless (number.to_i) return nil;
+   #return nil unless (number.to_i)
+   if (number.is_a? Integer) then
+     return number / 2
+   end
+
+   return nil
+end
+
+greet_programmer
+greet("Naureen")
+greet_with_default 'alice'
+add 3,25
+halve 326
+halve 'six'
